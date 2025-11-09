266a06b0933d53d845550f91eac6ff91c4c4199c
Authn <authn.sapuarachchi@gmail.com>
2025-11-09 19:43:20 +0530

    category section completly

--- .history/src/main/java/com/authcodelab/smartmoneymanageapp/service/CategoryService_20251109194255.java
+++ .history/src/main/java/com/authcodelab/smartmoneymanageapp/service/CategoryService_20251109194255.java
@@ -5,7 +5,7 @@ import com.authcodelab.smartmoneymanageapp.entity.CategoryEntity;
 import com.authcodelab.smartmoneymanageapp.entity.ProfileEntity;
 import com.authcodelab.smartmoneymanageapp.repository.CategoryRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
+
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -19,7 +19,7 @@ public class CategoryService {
     private final ProfileService profileService;
     private final CategoryRepository categoryRepository;
 
-    // save category
+    //save category
     public CategoryDTO saveCategory(CategoryDTO categoryDTO) {
         ProfileEntity profile = profileService.getCurrentProfile();
         if (categoryRepository.existsByNameAndProfileId(categoryDTO.getName(), profile.getId())) {
@@ -31,14 +31,14 @@ public class CategoryService {
         return toDTO(newCategory);
     }
 
-    // get categories for current users
+    //get categories for current users
     public List<CategoryDTO> getCategoriesForCurrentUser() {
         ProfileEntity profile = profileService.getCurrentProfile();
         List<CategoryEntity> categories = categoryRepository.findByProfileId(profile.getId());
         return categories.stream().map(this::toDTO).toList();
     }
 
-    // get category by type for current user
+    //get category by type for current user
     public List<CategoryDTO> getCategoryByTypeForCurrentUser(String type) {
         ProfileEntity profile = profileService.getCurrentProfile();
         List<CategoryEntity> categories = categoryRepository.findByTypeAndProfileId(type, profile.getId());
@@ -57,7 +57,7 @@ public class CategoryService {
         return toDTO(existingCategory);
     }
 
-    // helper methods
+    //helper methods
     private CategoryEntity toEntity(CategoryDTO categoryDTO, ProfileEntity profile) {
         return CategoryEntity.builder()
                 .name(categoryDTO.getName())
@@ -78,5 +78,9 @@ public class CategoryService {
                 .updatedAt(categoryEntity.getUpdatedAt())
                 .build();
     }
+
+
+
+
 
 }
